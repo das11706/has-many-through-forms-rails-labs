743b4fc567a6af8e38c58c18c7b6baadd819572b
Daniel Suarez <suarez.daniel101@gmail.com>
2022-12-20 23:56:44 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,16 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attributes|
       category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(category: category)
+      self.categories << category
+      # self.post_categories.build(category: category)
     end
   end
 
   def comments_attributes=(comment_attributes)
     comment_attributes.values.each do |comment_attributes|
       comment = Comment.find_or_create_by(comment_attribute)
-      self.post_comments.build(comment: comment)
+      self.comments << comment
+      # self.post_comments.build(comment: comment)
     end
   end
 
